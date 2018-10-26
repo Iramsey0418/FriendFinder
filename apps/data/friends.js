d0d951472296511f94355818aef36fb0b764372f
isaiah <isaiah.ramsey18@gmail.com>
2018-10-26 01:00:04 -0500

    Changes per

---
+++ apps/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Eminem',
+		photo: 'https://djbooth.net/.image/t_share/MTUzNDg1OTkyMDE3MjA4NTE4/eminem-wow-single-reactionjpg.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Project Pat',
+		photo: 'https://sslb.ulximg.com/image/405x405/artist/1294966878_e7f93226e624bd73383f731658a160fd.jpg/ec92cf33bd2e30199a0fd5f56c6951fd/1294966878_projectpat.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ciara',
+		photo: 'https://www.rap-up.com/app/uploads/2010/11/ciara-basic.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: '2Pac',
+		photo: 'https://i.4pcdn.org/pol/1398737098208s.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Cardi B',
+		photo: 'http://thesource.com/wp-content/uploads/2017/11/Cardi-B.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Black Friday MOM',
+		photo: 'https://i.ytimg.com/vi/p_SbKSe74Tc/maxresdefault.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Juicy J',
+		photo: 'https://djbooth.net/.image/t_share/MTUzNDg1OTkyODI0NjEyMDM4/juicy-j-says-to-stay-indiejpg.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'LL COOL J',
+		photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NjYxMzUxNDQ2MTI4MjM3/ll_cool_j_photo_by_chris_carroll_corbis_entertainment_getty_528656532.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dwayne Johnson',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/220px-Dwayne_Johnson_2%2C_2013.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Megan Fox',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Megan_Fox.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
